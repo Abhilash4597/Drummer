889bf84d437a48a2b411feca0fd4ba9ff198e3cc
Abhilash4597 <abhilash4597@gmail.com>
2023-02-09 01:15:07 +0530

    js code more optimization done

--- script.js
+++ script.js
@@ -9,70 +9,26 @@ const mMusic = new Audio('sounds/tom-1.mp3');
 const iMusic = new Audio('sounds/tom-2.mp3');
 const nMusic = new Audio('sounds/tom-3.mp3');
 const gMusic = new Audio('sounds/tom-4.mp3');
-
+    
 // # for clicking on box
 
-function checkClick(e) {
-  let click = e.target.innerText;
-
-  switch (click) {
-    case 'D':
-      dMusic.play();
-      break;
-
-    case 'R':
-      rMusic.play();
-      break;
-
-    case 'U':
-      uMusic.play();
-      break;
-
-    case 'M':
-      mMusic.play();
-      break;
-
-    case 'I':
-      iMusic.play();
-      break;
-
-    case 'N':
-      nMusic.play();
-      break;
-
-    case 'G':
-      gMusic.play();
-      break;
-
-    default:
-      console.log('wrong box clicked');
-  }
-}
-
 drum.forEach(element => {
-  element.addEventListener('click',checkClick)
-});
-
-// # For music play and pause
-
-let randomNum = Math.floor(Math.random() * 10) + 1;
-// console.log('music' + randomNum + '.mp3');
-let musics = new Audio('musics/music' + randomNum + '.mp3');
-
-play.addEventListener('click', () => {
-  musics.play();
-});
-
-pause.addEventListener('click', () => {
-  musics.pause();
+  element.addEventListener('click', (e) => {
+    playMusic(e.target.innerText.toLowerCase());
+  })
 });
 
 // # for pressing keys value 
 
-function checkKeyPress(e) {
-  let keys = e.key;
+document.addEventListener('keypress', (e) => {
+  playMusic(e.key);
+})
 
-  switch (keys) {
+// # Main function for playing sounds
+
+function playMusic(element) {
+
+  switch (element) {
     case 'd':
       dMusic.play();
       break;
@@ -89,21 +45,33 @@ function checkKeyPress(e) {
       mMusic.play();
       break;
 
-    case 'i':
+      case 'i':
       iMusic.play();
       break;
-
+      
     case 'n':
       nMusic.play();
       break;
-
+      
     case 'g':
       gMusic.play();
       break;
-    
+      
     default:
       console.log('wrong key pressed')
   }
 }
 
-document.addEventListener('keypress', checkKeyPress)
+// # For music play and pause
+
+let randomNum = Math.floor(Math.random() * 10) + 1;
+// console.log('music' + randomNum + '.mp3');
+let musics = new Audio('musics/music' + randomNum + '.mp3');
+
+play.addEventListener('click', () => {
+  musics.play();
+});
+
+pause.addEventListener('click', () => {
+  musics.pause();
+});
