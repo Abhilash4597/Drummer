8822b35e4048c534c081d4e405ed1851ad5ceec0
Abhilash4597 <abhilash4597@gmail.com>
2023-02-08 15:11:10 +0530

    added sound in all alphabets

---
+++ script.js
@@ -0,0 +1,58 @@
+const d = document.querySelector('.d');
+const r = document.querySelector('.r');
+const u = document.querySelector('.u');
+const m = document.querySelector('.m');
+const M = document.querySelector('.M');
+const e = document.querySelector('.e');
+const R = document.querySelector('.R');
+const drum = document.querySelectorAll('.drum');
+
+const dMusic = new Audio('sounds/crash.mp3');
+const rMusic = new Audio('sounds/kick-bass.mp3');
+const uMusic = new Audio('sounds/snare.mp3');
+const mMusic = new Audio('sounds/tom-1.mp3');
+const MMusic = new Audio('sounds/tom-2.mp3');
+const eMusic = new Audio('sounds/tom-3.mp3');
+const RMusic = new Audio('sounds/tom-4.mp3');
+
+function click_d() {
+  dMusic.play();
+};
+
+function click_r() {
+  rMusic.play();
+};
+
+function click_u() {
+  uMusic.play();
+};
+
+function click_m() {
+  mMusic.play();
+};
+
+function click_M() {
+  MMusic.play();
+};
+
+function click_e() {
+  eMusic.play();
+};
+
+function click_R() {
+  RMusic.play();
+};
+
+d.addEventListener('click', click_d);
+
+r.addEventListener('click', click_r);
+
+u.addEventListener('click', click_u);
+
+m.addEventListener('click', click_m);
+
+M.addEventListener('click', click_M);
+
+e.addEventListener('click', click_e);
+
+R.addEventListener('click', click_R);
